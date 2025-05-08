37bb7bc4dd9b8d2278cd0639711d198b17616684
ranamagararjun <126785168+ranamagararjun@users.noreply.github.com>
2025-05-08 14:56:40 +0100

    Updated MainActivity.kt

--- app/src/main/java/com/example/retrofitdogapp/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdogapp/MainActivity.kt
@@ -3,27 +3,31 @@ package com.example.retrofitdogapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.retrofitdogapp.screens.DogScreen
+import com.example.retrofitdogapp.screens.DogInfoScreen
 import com.example.retrofitdogapp.ui.theme.RetroFitDogAppTheme
+import com.example.retrofitdogapp.viewmodel.DogViewModel
 
 class MainActivity : ComponentActivity() {
+    private val dogViewModel: DogViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             RetroFitDogAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    AppNavigation(navController = navController, dogViewModel = dogViewModel)
                 }
             }
         }
@@ -31,17 +35,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RetroFitDogAppTheme {
-        Greeting("Android")
+fun AppNavigation(navController: NavHostController, dogViewModel: DogViewModel) {
+    NavHost(navController = navController, startDestination = "dog_screen") {
+        composable("dog_screen") {
+            DogScreen(
+                dogViewModel = dogViewModel,
+                onDogSelected = { dogId ->
+                    navController.navigate("dog_info_screen/$dogId")
+                }
+            )
+        }
+        composable("dog_info_screen/{dogId}") { backStackEntry ->
+            val dogId = backStackEntry.arguments?.getString("dogId")
+            DogInfoScreen(dogId = dogId, dogViewModel = dogViewModel)
+        }
     }
 }
